e1d2c8233651c83364351a692cb421295c40538f
ligi <ligi@ligi.de>
2015-05-21 01:10:17 +0200

    Add howtoish thing

--- app/src/main/java/org/ligi/etheremote/ConnectionSettingsActivity.java
+++ app/src/main/java/org/ligi/etheremote/ConnectionSettingsActivity.java
@@ -2,6 +2,8 @@ package org.ligi.etheremote;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -18,9 +20,17 @@ import org.jetbrains.annotations.NotNull;
 
 public class ConnectionSettingsActivity extends AppCompatActivity {
 
+    @InjectView(R.id.help)
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conn_settings);
+
+        ButterKnife.inject(this);
+
+        textView.setText(Html.fromHtml(getText(R.string.connection_help).toString()));
+        textView.setMovementMethod(new LinkMovementMethod());
     }
 }
