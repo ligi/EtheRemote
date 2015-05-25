e180beae92bcf3c8e8d6407aa9e5db2e47301d8e
ligi <ligi@ligi.de>
2015-05-25 14:36:06 +0200

    Extract communicator and read peer count

--- app/src/main/java/org/ligi/etheremote/App.java
+++ app/src/main/java/org/ligi/etheremote/App.java
@@ -5,15 +5,21 @@ import android.app.Application;
 public class App extends Application {
 
     private static Settings settings;
+    private static EthereumCommunicator communicator;
 
     @Override
     public void onCreate() {
         super.onCreate();
         settings=new Settings(this);
+        communicator = new EthereumCommunicator();
     }
 
     public static Settings getSettings() {
         return settings;
+    }
+
+    public static EthereumCommunicator getCommunicator() {
+        return communicator;
     }
 
 }
--- app/src/main/java/org/ligi/etheremote/MainActivity.java
+++ app/src/main/java/org/ligi/etheremote/MainActivity.java
@@ -5,15 +5,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import com.github.arteam.simplejsonrpc.client.JsonRpcClient;
-import com.github.arteam.simplejsonrpc.client.Transport;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-import java.io.IOException;
-import org.jetbrains.annotations.NotNull;
 
 
 public class MainActivity extends EtheremoteActivity {
@@ -21,13 +12,13 @@ public class MainActivity extends EtheremoteActivity {
     @InjectView(R.id.currentBlock)
     TextView textView;
 
+    @InjectView(R.id.peerCount)
+    TextView peerCountTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ButterKnife.inject(this);
-
 
         if (App.getSettings().getOnboardingState() == Settings.ONBOARDING_NONE) {
             startActivity(new Intent(this, ConnectionSettingsActivity.class));
@@ -36,36 +27,28 @@ public class MainActivity extends EtheremoteActivity {
             return;
         }
 
+        ButterKnife.inject(this);
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                final OkHttpClient client = new OkHttpClient();
 
-                final JsonRpcClient rpcClient = new JsonRpcClient(new Transport() {
+                while (true) {
+                    try {
+                        final int blockNumber = App.getCommunicator().getBlockNumber();
+                        final Integer peerCount = App.getCommunicator().getPeerCount();
 
-                    @NotNull
-                    @Override
-                    public String pass(@NotNull String request2) throws IOException {
-                        final Settings settings = App.getSettings();
-                        Request request = new Request.Builder().url("http://" + settings.getHost() + ":" + settings.getPort())
-                                                               .post(RequestBody.create(MediaType.parse("text"), request2))
-                                                               .build();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText("#" + blockNumber);
+                                peerCountTV.setText("" + peerCount);
+                            }
+                        });
+                        Thread.sleep(300);
+                    } catch (Exception e) {
 
-                        Response response = client.newCall(request).execute();
-                        return response.body().string();
                     }
-                });
-
-                try {
-                    final String res = rpcClient.createRequest().method("eth_blockNumber").id(1).execute().toString();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            textView.setText("" + Integer.parseInt(res.replace("0x", ""), 16));
-                        }
-                    });
-                } catch (Exception e) {
-
                 }
             }
         }).start();
